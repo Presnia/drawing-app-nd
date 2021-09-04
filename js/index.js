339490e9af8e6a986106e3834934fc2244e50a80
Andrei Dzebysh <presnia@yahoo.com>
2021-09-04 15:41:45 +0300

    All functionality is implemented and styles is a bit corrected

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const decreaseBtn = document.getElementById('decrease');
+const increaseBtn = document.getElementById('increase');
+const clearBtn = document.getElementById('clear');
+const sizeEl = document.getElementById('size');
+const inputColor = document.getElementById('color');
 
 let size = 20;
 let isPressed = false;
@@ -48,3 +53,29 @@ function drawLine(x1, y1, x2, y2) {
   ctx.lineWidth = size * 2;
   ctx.stroke();
 }
+
+decreaseBtn.addEventListener('click', () => {
+  let res = Number(sizeEl.innerText) - 5;
+
+  if (res > 0) {
+    sizeEl.innerText = res;
+    size = res;
+  }
+});
+
+increaseBtn.addEventListener('click', () => {
+  let res = Number(sizeEl.innerText) + 5;
+
+  if (res <= 50) {
+    sizeEl.innerText = res;
+    size = res;
+  }
+});
+
+inputColor.addEventListener('change', () => {
+  color = inputColor.value;
+});
+
+clearBtn.addEventListener('click', () => {
+  document.location.reload();
+});
